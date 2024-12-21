2c3783ded5b39132d92b61ec01e906e2a8ee75a8
Daniel <dafernandezperez@gmail.com>
2024-12-21 17:38:48 +0100

    Progress

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,40 @@
-use chrono::{NaiveDate, NaiveDateTime};
+use chrono::NaiveDateTime;
+use polars::prelude::{DataFrame};
+  
+#[derive(Debug)] 
+struct Position {
+    iv: f64,
+    av: f64,
+    y: f64,
+}
+
+#[derive(Debug)]
+struct Asset {
+    isin: String,
+    short_name: String,
+    name: String,
+    desc: String,
+    positions: Position, //Eventually vector of positions from different people
+    history_data: DataFrame,
+}
 
 enum Operation<'a> {
     Buy {
-        isin: &'a str,
         datetime: NaiveDateTime,
+        isin: &'a str,
         shares: f64,
         price: f64,
     },
     Sell {
-        isin: &'a str,
         datetime: NaiveDateTime,
+        isin: &'a str,
         shares: f64,
         price: f64,
     },
     Transfer {
+        datetime: NaiveDateTime,
         isin_from: &'a str,
         isin_to: &'a str,
-        datetime: NaiveDateTime,
         shares_from: f64,
         price_from: f64,
         shares_to: f64,
@@ -26,59 +44,67 @@ enum Operation<'a> {
 
 fn print_operations_header() {
     println!(
-        "\n{:<10} {:<12} {:<20} {:>12} {:>12}\n{}",
+        "\n{:<22} {:<10} {:<12} {:>12} {:>12}\n{}",
+        "DATE",
         "OP",
         "ISIN",
-        "DATE",
         "SHARES",
         "PRICE",
-        "=".repeat(70)
+        "=".repeat(72)
     );
 }
 
 fn print_operation(movement: Operation) {
     match movement {
         Operation::Buy {
-            isin,
             datetime,
+            isin,
             shares,
             price,
         } => {
             println!(
-                "{:<10} {:<12} {:<20} {:>12.4} {:>12.4}",
-                "BUY", isin, datetime, shares, price
+                "{:<22} {:<10} {:<12} {:>12.4} {:>12.4}",
+                datetime.to_string(),
+                "BUY",
+                isin,
+                shares,
+                price
             );
         }
         Operation::Sell {
-            isin,
             datetime,
+            isin,
             shares,
             price,
         } => {
             println!(
-                "{:<10} {:<12} {:<20} {:>12.4} {:>12.4}",
-                "SELL", isin, datetime, shares, price
+                "{:<22} {:<10} {:<12} {:>12.4} {:>12.4}",
+                datetime.to_string(),
+                "SELL",
+                isin,
+                shares,
+                price
             );
         }
         Operation::Transfer {
+            datetime,
             isin_from,
             isin_to,
-            datetime,
             shares_from,
             price_from,
             shares_to,
             price_to,
         } => {
             println!(
-                "{:<10} {:<12} {:<20} {:>12.4} {:>12.4}\n{:<10} {:<12} {:<20} {:>12.4} {:>12.4}",
+                "{:<22} {:<10} {:<12} {:>12.4} {:>12.4}\n{:<22} {:<10} {:<12} {:>12.4} {:>12.4}",
+                datetime.to_string(),
                 "SEND",
                 isin_from,
-                datetime,
                 shares_from,
                 price_from,
+                datetime.to_string(),
                 "RECEIVE",
                 isin_to,
-                datetime,
                 shares_to,
                 price_to
             );
@@ -88,24 +114,24 @@ fn print_operation(movement: Operation) {
 
 fn main() {
     let test_buy = Operation::Buy {
-        isin: "AAPL",
         datetime: NaiveDateTime::parse_from_str("2024-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
             .expect("Error"),
+        isin: "AAPL",
         shares: 2.23,
         price: 303.23467,
     };
     let test_sell = Operation::Sell {
-        isin: "SP500",
         datetime: NaiveDateTime::parse_from_str("2024-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
             .expect("Error"),
+        isin: "SP500",
         shares: 129.3,
         price: 10287.2764128,
     };
     let test_transfer = Operation::Transfer {
-        isin_from: "IE7632X23",
-        isin_to: "LU7826315",
         datetime: NaiveDateTime::parse_from_str("2024-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
             .expect("Error"),
+        isin_from: "IE7632X23",
+        isin_to: "LU7826315",
         shares_from: 23.221,
         price_from: 105.214,
         shares_to: 0.12,
