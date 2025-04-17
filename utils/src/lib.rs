98009f953f6d197cf06d8a15e6c57097ab254335
dafer-t480 <dafernandezperez@gmail.com>
2025-04-17 15:12:52 +0200

    Renamed get_cwd function

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,6 @@
 use std::env;
 use std::path::PathBuf;
 
-pub fn get_project_cwd() -> PathBuf {
+pub fn get_cwd() -> PathBuf {
     env::current_dir().expect("Failed to get current working directory")
 }
