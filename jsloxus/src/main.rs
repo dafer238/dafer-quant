25e9b65e0ea77fa6694ba029fe0415cf73573a4e
dafer-wsl-sener <dafernandezperez@gmail.com>
2025-05-09 14:49:08 +0200

    Updates on frontend

--- jsloxus/src/main.rs
+++ jsloxus/src/main.rs
@@ -5,6 +5,7 @@ use dioxus::prelude::*;
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const HEADER_LOGO: Asset = asset!("/assets/header_logo.svg");
 
 fn main() {
     dioxus::launch(App);
@@ -24,16 +25,19 @@ fn App() -> Element {
 pub fn Hero() -> Element {
     rsx! {
         div {
-            id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
-            }
+            id: "main_menu",
+            img {src: HEADER_LOGO, id:"header_logo"}
+            a { href: "", "Home"}
+            a { href: "", "About"}
+        }
+        div {
+            id: "links",
+            a { href: "https://dioxuslabs.com/learn/0.6/", "Learn Dioxus" }
+            a { href: "https://dioxuslabs.com/awesome", "Awesome Dioxus" }
+            a { href: "https://github.com/dioxus-community/", "Community Libraries" }
+            a { href: "https://github.com/DioxusLabs/sdk", "Dioxus Development Kit" }
+            a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "VSCode Extension" }
+            a { href: "https://discord.gg/XgGxMSkvUM", "Community Discord" }
         }
     }
 }
